1eecc464e4eb082c9a1f32464f00ae6cc65a7f51
XxSusuxX <gabriel.hneus@gmail.com>
2023-10-04 18:38:00 -0300

    firts commit

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less:{
+            development:{
+                files:{
+                    'main.css': 'main.less'
+                }
+            },
+            production:{
+                options:{
+                    compress: true,
+                },
+                files:{
+                    'main.min.css': 'main.less'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less')
+
+    grunt.registerTask('default',[
+        'less'
+    ])
+}
